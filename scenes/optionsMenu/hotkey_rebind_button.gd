19bafa006d15bfe31164eeb792baa54fe89052fe
raidoon <thomaselphin@gmail.com>
2024-08-26 15:30:27 +0200

    adding keybounds

---
+++ scenes/optionsMenu/hotkey_rebind_button.gd
@@ -0,0 +1,30 @@
+class_name HotKeyRebindButton
+extends Control
+
+@onready var label: Label = $HotKeyContainer/hotkey_name
+@onready var button: Button = $HotKeyContainer/hotkey_bt
+
+@export var action_name : String = "move_left"
+
+func _ready():
+	set_process_unhandled_key_input(false)
+	set_action_name()
+	set_text_for_key()
+
+func set_action_name() -> void:
+	label.text = "Unassigned"
+	
+	match action_name:
+		"move_left":
+			label.text = "Move Left"
+		"move_right":
+			label.text = "Move Right"
+		"jump":
+			label.text = "Jump"
+
+func set_text_for_key() -> void:
+	var action_events = InputMap.action_get_events(action_name)
+	var action_event = action_events[0]
+	var action_keycode = OS.get_keycode_string(action_event.physical_keycode)
+	
+	button.text = "%s" % action_keycode
