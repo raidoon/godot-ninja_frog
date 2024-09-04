db401de3aa9a68f93454e3b36b0e9f8badd663d0
raidoon <thomaselphin@gmail.com>
2024-09-04 09:57:49 +0200

    main sound slider doesnt work

--- scenes/optionsMenu/sound_control.gd
+++ scenes/optionsMenu/sound_control.gd
@@ -1,15 +1,18 @@
-extends HSlider
+#extends HSlider
 #EZ A MAIN VOLUME SLIDER !!!!!
-@export
-var bus_name: String
-var bus_index: int
+#@export
+#var bus_name: String
+#var bus_index: int
 
-func _ready()->void:
-	bus_index=AudioServer.get_bus_index(bus_name)
-	value_changed.connect(_on_h_slider_value_changed)
+#func _ready()->void:
+#	bus_index=AudioServer.get_bus_index(bus_name)
+#	value_changed.connect(_on_h_slider_value_changed)
 
 
-func _on_h_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
+#func _on_h_slider_value_changed(value: float) -> void:
+#	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
+#	AudioServer.set_bus_mute(bus_index,value<.05)
+
+
 
 #HIBA: INDEXNEK NINCS DEF ÉRTÉKE, AHOGY A BUS_NAME-NEK SEM, EZÉRT AD VISSZA -1-ET
