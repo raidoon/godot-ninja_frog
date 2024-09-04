193a5334ca4683a05127aea3d59ce8c952082d83
raidoon <thomaselphin@gmail.com>
2024-09-04 09:46:57 +0200

    trying to get the volume sliders to work

--- scenes/optionsMenu/sound_control.gd
+++ scenes/optionsMenu/sound_control.gd
@@ -1,7 +1,15 @@
-extends Control
+extends HSlider
+#EZ A MAIN VOLUME SLIDER !!!!!
+@export
+var bus_name: String
+var bus_index: int
 
-@onready var sound_slider = $HBoxContainer/HSlider as HSlider
+func _ready()->void:
+	bus_index=AudioServer.get_bus_index(bus_name)
+	value_changed.connect(_on_h_slider_value_changed)
 
 
-func _ready() -> void:
-	pass
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus_index,linear_to_db(value))
+
+#HIBA: INDEXNEK NINCS DEF ÉRTÉKE, AHOGY A BUS_NAME-NEK SEM, EZÉRT AD VISSZA -1-ET
