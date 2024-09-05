98bd6d48df0b4377f5e2df0f9fc7274f3728948c
raidoon <thomaselphin@gmail.com>
2024-09-05 12:33:08 +0200

    music and master sound sliders are done

--- scenes/optionsMenu/music_volume_slider.gd
+++
@@ -1,7 +0,0 @@
-extends HSlider
-
-@onready var music = AudioServer.get_bus_index("music")
-
-func _on_h_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(music,linear_to_db(value))
-	AudioServer.set_bus_mute(music,value<.05)
